464d47d3ff4ebfb437501f1b68203e053c4a768d
unknown <snaikons@outlook.com>
2021-06-30 12:05:14 +0300

    first commit

---
+++ 4 masala.py
@@ -0,0 +1,7 @@
+qidirilayotgan_son = int(input('qidirilayotgan raqam kiriting:'))
+for i in range(1, 20):
+    if qidirilayotgan_son == i:
+        print('bor')
+        break
+else:
+    print('yo`q')
---
+++ main.py
@@ -0,0 +1,11 @@
+ism = 'Sunnatillo'
+
+index = 0
+while index<len(ism):
+    print(ism[index])
+    index +=1
+
+
+# forda bajarish
+for i in ism:
+    print(i)
