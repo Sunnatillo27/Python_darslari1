de1402b602dccad034bc94e19ff8cba20d21184e
Sunnatillo27 <86454272+Sunnatillo27@users.noreply.github.com>
2021-07-01 10:11:23 +0500

    01.07.2021

--- 1-masala.py
+++ 1-masala.py
@@ -1,11 +1,4 @@
-# forda rangedan foydalanish
-# for yordamida 1 dan 10 gacha sonlarni chiqarish
-# range qo'shmasdan foydalanish
-
-sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-kunlar = ['Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba']
-for son in sonlar:
-    print(son)
-
-for kun in kunlar:
-    print(kun)
+numbers = [2, 1, 6, 7] # sechi son bor so'lsa shuncha marta chiqarish dasturi
+for number in numbers:
+        for i in range(number):
+                print(number)
---
+++ 2 masala.py
@@ -0,0 +1,6 @@
+sonlar = [2, 8, 4, 5, 4, 1, 0, 9]
+
+for index in range(len(sonlar)):
+    for key in range(len(sonlar)):
+        if sonlar[index] < sonlar
+            sonlar[index]=sonlar[key]+sonlar[index]
