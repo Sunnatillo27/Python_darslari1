025a9ff71fcb9d8b41cf13e41a7ee9ef1dbd6516
Sunnatillo27 <86454272+Sunnatillo27@users.noreply.github.com>
2021-07-01 10:15:39 +0500

    Update 2 masala.py

--- 2 masala.py
+++ 2 masala.py
@@ -1,6 +1,9 @@
-sonlar = [2, 8, 4, 5, 4, 1, 0, 9]
+sonlar = [8, 4, 5, 4, 1, 0, 9]
 
 for index in range(len(sonlar)):
     for key in range(len(sonlar)):
-        if sonlar[index] < sonlar
+        if sonlar[index] < sonlar[key]:
             sonlar[index]=sonlar[key]+sonlar[index]
+            sonlar[key] = sonlar[index] - sonlar[key]
+            sonlar[index] = sonlar[index] - sonlar[key]
+            print(sonlar)
